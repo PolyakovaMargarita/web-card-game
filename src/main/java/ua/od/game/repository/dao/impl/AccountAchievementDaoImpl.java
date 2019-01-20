fbfc016af4e6bf5995c1f6c302c73982f8e4a2e0
OlegButskyi <xjustted@gmail.com>
2019-01-20 07:14:18 +0200

    AccountAchievement DAO SERVICE and CONTROLLER Test's doesn't work

---
+++ src/main/java/ua/od/game/repository/dao/impl/AccountAchievementDaoImpl.java
@@ -0,0 +1,57 @@
+package ua.od.game.repository.dao.impl;
+
+import ua.od.game.model.AccountAchievementEntity;
+import ua.od.game.repository.dao.AccountAchievementDao;
+import ua.od.game.repository.helper.SqlHelper;
+
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AccountAchievementDaoImpl implements AccountAchievementDao {
+
+    private static final String GET_ACCOUNT_ACHIEVEMENT_LIST = "SELECT * FROM Account_Achievement WHERE account_id = ?";
+ // private static final String GET_ACCOUNT_ACHIEVEMENT_LIST_1 = "SELECT * FROM Account_Achievement WHERE account_id = ";
+    private static final String ADD_ACHIEVEMETN_TO_ACCOUNT = "";
+
+
+//    @Override
+//    public List<AccountAchievementEntity> getUserAchievementsList(Integer accountId) {
+//        accountId.toString();
+//        String getAccountAchievement = GET_ACCOUNT_ACHIEVEMENT_LIST_1 + accountId;
+//        return SqlHelper.prepareStatement(getAccountAchievement, query -> {
+//            ResultSet resultSet = query.executeQuery();
+//            List<AccountAchievementEntity> result = new LinkedList<>();
+//            while (resultSet.next()) {
+//                result.add(new AccountAchievementEntity() {{
+//                    setId(resultSet.getInt("id"));
+//                    setAmount(resultSet.getFloat("amount"));
+//                    setAchievementId(resultSet.getInt("achievement_id"));
+//                    setAccountAchievementId(resultSet.getInt("account_id"));
+//                }});
+//            }
+//            return result;
+//        });
+//    }
+
+        @Override
+    public List<AccountAchievementEntity> getUserAchievementsList(Integer accountId) {
+        return SqlHelper.prepareStatement(GET_ACCOUNT_ACHIEVEMENT_LIST, query -> {
+            query.setInt(1,accountId);
+            ResultSet resultSet = query.executeQuery();
+            List<AccountAchievementEntity> result = new LinkedList<>();
+            while (resultSet.next()) {
+                result.add(new AccountAchievementEntity() {{
+                    setId(resultSet.getInt("id"));
+                    setAccountAchievementId(resultSet.getInt("account_id"));
+                    setAchievementId(resultSet.getInt("achievement_id"));
+                    setAmount(resultSet.getFloat("amount"));
+                }});
+            }
+            return result;
+        });
+    }
+    public List<AccountAchievementEntity> addAchievementToAccount(Integer accountId){
+        return null;
+    }
+}
