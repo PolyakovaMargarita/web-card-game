516040cd0ce270713eb46f4b7f6bc288ae0ab51b
OlegButskyi <xjustted@gmail.com>
2019-02-06 19:21:22 +0200

    Fill the Database

--- src/test/resources/db/08_Achievements.sql
+++ src/test/resources/db/08_Achievements.sql
@@ -1,5 +1,15 @@
 INSERT INTO `Achievement`
-    (`name`, `description`)
+    (`name`, `description`,`resource_set_id`)
 VALUES
-    ('Power of bottle','You are extremely lucky int the search for bootles'),
-    ('cigarette crank', 'you got so much smoke that sigarettes dont kill you');
+    ('Power of bottle','You are extremely lucky int the search for bootles', 1),
+    ('cigarette crank', 'you got so much smoke that sigarettes dont kill you', 2);
+INSERT INTO `Achievement`
+    (`name`, `description`,`building_set_id`)
+VALUES
+    ('','', 1),
+    ('', '', 2);
+INSERT INTO `Achievement`
+    (`name`, `description`,`upgrade_set_id`)
+VALUES
+    ('','', 1),
+    ('', '', 2);
